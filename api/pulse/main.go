93fc4159acadb9eebffcb6b5f9649d87880a5444
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-12-19 14:31:13 +0100

    .

--- api/pulse/main.go
+++ api/pulse/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+
+	"net/http"
+
+	"github.com/gocolly/colly"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// get all urls from https://news.ycombinator.com/newest
+func getHnNewestUrls(last int) {
+	fmt.Println(last, "last")
+}
 
 func main() {
-	fmt.Println("testing")
+	fmt.Println("wow")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/hn-newest/{last}", func(w http.ResponseWriter, r *http.Request) {
+		lastStr := chi.URLParam(r, "last")
+		last, err := strconv.Atoi(lastStr)
+		if err != nil {
+			http.Error(w, "Invalid parameter", http.StatusBadRequest)
+			return
+		}
+		getHnNewestUrls(last)
+		w.Write([]byte("welcome"))
+	})
+	http.ListenAndServe(":3000", r)
+
+	c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("http://go-colly.org/")
 }
--- api/pulse/readme.md
+++ api/pulse/readme.md
@@ -1,3 +1,5 @@
 # Pulse
 
+> Scrape websites like HN/lobsters/..
+
 Everything described in [this issue](https://github.com/learn-anything/learn-anything.xyz/issues/91).
