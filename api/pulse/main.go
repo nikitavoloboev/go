877c34282239f471bf8e98d28b34605bdea0501e
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-12-19 15:31:04 +0100

    .

--- api/pulse/main.go
+++ api/pulse/main.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	util "github.com/nikitavoloboev/util"
 )
 
 // get all urls from https://news.ycombinator.com/newest
@@ -32,6 +33,7 @@ func getHnNewestUrls(last int) []string {
 }
 
 func main() {
+	util.ConvertToString(1)
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/hn-newest/{last}", func(w http.ResponseWriter, r *http.Request) {
